7ee2f0210af9de2cdb7492ecaa209f874748c4ba
Anders Håkansson <andy@a904guy.com>
2014-07-23 09:37:12 -0400

    Forgot debug ;)

--- src/pgos.php
+++ src/pgos.php
@@ -4,7 +4,6 @@ spl_autoload_register(function ($class) {
     if(is_numeric(strpos($class,'pgos'))) return False;
     
     $name = strtolower(str_replace('PGOS_','',$class));
-    var_dump($name);
     $path = __DIR__.'/adapter/'.$name.'.adapter.php';
     if(is_file($path))
         return include($path);
